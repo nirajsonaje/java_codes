ff422cc7b294f5e3f50c55895fe5424a0e7cc989
Niraj sonaje <101004260+nirajsonaje@users.noreply.github.com>
2024-10-17 07:37:26 +0530

    Create ReverseVowelsofaString.java

---
+++ ReverseVowelsofaString.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
+        String vowels = "aeiouAEIOU";
+
+        while (left < right) {
+            // Move the left pointer to the right until it points to a vowel.
+            while (left < right && vowels.indexOf(chars[left]) == -1) {
+                left++;
+            }
+
+            // Move the right pointer to the left until it points to a vowel.
+            while (left < right && vowels.indexOf(chars[right]) == -1) {
+                right--;
+            }
+
+            // Swap the vowels at left and right positions.
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(chars);
+    }
+}
