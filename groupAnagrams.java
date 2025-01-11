f84f2c92a43e933464d27892e9a1a1c65b1f8f2f
Niraj sonaje <101004260+nirajsonaje@users.noreply.github.com>
2025-01-11 18:23:25 +0530

    Update groupAnagrams.java

--- groupAnagrams.java
+++ groupAnagrams.java
@@ -4,13 +4,17 @@ class Solution {
             return new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-            char[] ca = s.toCharArray();
+            char[] ca = s.toCharArray(); //a e t //a e t
             Arrays.sort(ca);
             String keyStr = String.valueOf(ca);
-            if (!map.containsKey(keyStr))
-                map.put(keyStr, new ArrayList<>());
-            map.get(keyStr).add(s);
+            if (!map.containsKey(keyStr)){
+                map.put(keyStr, new ArrayList<>()); //aet
+            }
+            map.get(keyStr).add(s);//
         }
         return new ArrayList<>(map.values());
     }
 }
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
